bdd862f91edb8c8762d349ed9bb9e05d1a447e16
nardtree <gim.kobayashi@gmail.com>
2017-11-29 22:22:59 +0900

    ab

--- user_works.py
+++ user_works.py
@@ -12,32 +12,49 @@ import json
 
 from multiprocessing import Process
 
-def _map(name):
+import os
 
-  html, links = pickle.loads( gzip.decompress( open(name, 'rb').read() ))
-  soup = bs4.BeautifulSoup(html, 'lxml')
-  
-  user = soup.find('p', {'id': 'user-name-userId'})
-  if user is None:
-    return
-  user = user.text
+import sys
+def _map(arr):
+  key, names = arr
+  for name in names:
+    html, links = pickle.loads( gzip.decompress( open(name, 'rb').read() ))
+    soup = bs4.BeautifulSoup(html)
+    
+    user = soup.find('p', {'id': 'user-name-userId'})
+    if user is None:
+      continue
+    user = user.text
 
-  works = []
-  for h4 in soup.find_all('h4', {'class':'widget-workCatchphrase-title'}):
-    works.append( re.sub(r'\s{1,}', ' ', h4.text)  )
+    if os.path.exists(f'user_works/{user}.json') is True:
+      continue
+    works = []
+    for h4 in soup.find_all('h4', {'class':'widget-workCatchphrase-title'}):
+      works.append( re.sub(r'\s{1,}', ' ', h4.text)  )
 
-  print(name)
-  print('finished', user)
-  open(f'user_works/{user}.json', 'w').write( json.dumps({'user':user, 'works':works}, indent=2, ensure_ascii=False) )
+    print(name)
+    print('index', key, 'finished', user)
+    open(f'user_works/{user}.json', 'w').write( json.dumps({'user':user, 'works':works}, indent=2, ensure_ascii=False) )
 
+if '--init' in sys.argv:
+  arrs = {}
+  for index, name in enumerate(glob.glob('htmls/*')):
+    if re.search(r'following_works.pkl.gz$', name) is None:
+      continue
+    key = index%16
+    if arrs.get(key) is None:
+      arrs[key] = []
+    arrs[key].append( name )
+  arrs = [(key, names) for key, names in arrs.items() ]
+  open('user_books_arrs.json', 'w').write( json.dumps(arrs, indent=2) )
+  sys.exit()
+
+arrs = json.loads(open('user_books_arrs.json').read() )
+#_map(arrs[0])
 ps = []
-for name in glob.glob('htmls/*'):
-  if re.search(r'following_works.pkl.gz$', name) is None:
-    continue
-  p = Process(target=_map, args=(name,))
+for arr in arrs:
+  p = Process(target=_map, args=(arr,))
   p.start()
   ps.append(p)
-  if len(ps) > 32:
-    [p.join() for p in ps]
-    ps = []
+[p.join() for p in ps]
   
--- work_users_chaine.py
+++ work_users_chaine.py
@@ -3,6 +3,16 @@ import json
 
 import glob
 
+book_users = {}
 for name in glob.glob('user_works/*.json'):
   obj = json.loads(open(name).read() )
-  print( obj )
+  user = obj['user']
+  books = obj['works']
+
+  for book in books:
+    if book_users.get(book) is None:
+      book_users[book] = []
+    book_users[book].append(user)
+
+for book, users in book_users.items():
+  print(book, users)
